2cbb7451936bc46ba9b4f649f26781e2a21d978e
Saifur Rahman <saifur703@gmail.com>
2020-11-01 23:44:15 +0600

    Initial Deploy

---
+++ assets/js/active.js
@@ -0,0 +1,20 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function ($) {
+
+        $(function () {
+            $('.main-menu').slicknav();
+        });
+
+
+    });
+
+
+    jQuery(window).on("load", function () {
+
+    });
+
+
+
+})(jQuery);
